0dcf526bb1070a6c4c482cc63ca071796e9edf7d
John Edwards <edwards1860@gmail.com>
2024-12-12 22:48:56 -0800

    day11 done woo!

--- day11.jl
+++ day11.jl
@@ -1,25 +1,33 @@
-# global input = [8435, 234, 928434, 14, 0, 7, 92446, 8992692]
-global input = [0]
-global queue = []
-using ProgressBars
-for blink in 1:10
-    while length(input) > 0
-        x = pop!(input)
-        dig = length(digits(x))
-        if x == 0
-            x = 1
-            push!(queue, x)
-        elseif dig % 2 == 0
-            println(x)
-            x1 = floor(x / 10 ^ (dig/2)) 
-            x2 = x - x1 * 10 ^ (dig/2)
-            push!(queue, Int(x1))
-            push!(queue, Int(x2))
-        else
-            x = x * 2024
-            push!(queue, x)
-        end
+# input
+include("Utils.jl")
+using .Utils
+input = get_input(2024,11)[1]
+# part one
+function process_loop(x)
+    dig = length(digits(x))
+    if x == 0
+        return [1]
+    elseif dig % 2 == 0
+        return [Int(floor(x / 10 ^ (dig/2))), Int(x - floor(x / 10 ^ (dig/2)) * 10 ^ (dig/2))]
+    else
+        return [x * 2024]
     end
-    global input = copy(queue)
-    global queue = []
 end
+function my_loop(input, times)
+    loop_counts = Dict(i => sum(input .== i) for i in unique(input))
+    for n in 1:times
+        new_dict = Dict()
+        for x in keys(loop_counts)
+            count = loop_counts[x]
+            vals = process_loop(x)
+            for val in vals
+                new_dict[val] = get(new_dict, val, 0) + count
+            end
+        end
+        loop_counts = new_dict
+    end
+    return sum([loop_counts[x] for x in keys(loop_counts)])
+end
+println(my_loop(input, 25))
+# part two
+println(my_loop(input, 75))
