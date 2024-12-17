0572a7139f9366281ecc5ce4e6d5cf290266e127
John Edwards <edwards1860@gmail.com>
2024-12-17 11:20:17 -0800

    day 16 solved

--- day16.jl
+++ day16.jl
@@ -1,37 +1,48 @@
 # input 
 include("Utils.jl")
 using .Utils
-input = get_example(2024,16)
-input = split.(input,"")
-input = mapreduce(permutedims, vcat, input)
-input = [string(i) for i in input]
-directions = CartesianIndex.([(0,1),(-1,0),(0,-1),(1,0)])
+input = [string(i) for i in mapreduce(permutedims, vcat, split.(get_input(2024,16),""))]
 # part one 
-function _all_paths(input, u, d, visited, path)
-    visited[u] = true
-    append!(path, [u])
-    if u == d
-        println(path)
-    else
-        neighbors = [(u + j) for j in directions
-            if (input[u + j] != "#") & in((u + j), keys(visited))
-        ]
-        for n in neighbors
-            if !visited[n]
-                _all_paths(input, n, d, visited, path)
+directions = [(0,1),(-1,0),(0,-1),(1,0)]
+prev = Dict()
+start, target_node = (Tuple(findfirst(x->x==y, input)) for y in ["S","E"])
+vertices = Dict((x,y) => Inf for x in Tuple.(findall(x->x!="#", input)), y in directions)
+vertices[(start,(0,1))] = 0
+unvisited = copy(vertices)
+while length(unvisited) > 0
+    current_node, current_direction = findmin(unvisited)[2]
+    neighbors = [(current_node .+ j,j) for j in directions
+        if (input[current_node[1] + j[1], current_node[2] + j[2]] != "#") & 
+            in((current_node .+ j,j), keys(unvisited))
+    ]
+    for n in neighbors
+        old_dist = vertices[n]
+        if current_direction == n[2]
+            new_dist = vertices[(current_node, current_direction)] + 1
+        else
+            new_dist = vertices[(current_node, current_direction)] + 1001
+        end
+        if old_dist >= new_dist
+            vertices[n], unvisited[n] = new_dist, new_dist
+            if n in keys(prev)
+                push!(prev[n], (current_node, current_direction))
+            else
+                prev[n] = [(current_node, current_direction)]
             end
         end
     end
-    x = pop!(path)
-    visited[u] = false
+    delete!(unvisited, (current_node, current_direction))
 end
-
-function all_paths(input)
-    start = findfirst(x->x=="S",input)
-    dest = findfirst(x->x=="E",input)
-    vertices = findall(x->x!="#",input)
-    visited = Dict(v => false for v in vertices)
-    return _all_paths(input, start, dest, visited, [])
+println(minimum([vertices[(target_node,x)] for x in directions]))
+# part two
+path = []
+target_dir = directions[argmin([vertices[(target_node,x)] for x in directions])]
+tocheck = [(target_node,target_dir)]
+while tocheck != []
+    u = pop!(tocheck)
+    if in(u, keys(prev))
+        push!(path, u)
+        append!(tocheck, prev[u])
+    end
 end
-
-all_paths(input)
+println(length(unique([x[1] for x in path])) + 1)
