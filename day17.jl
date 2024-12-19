f319481d6c9893bdcf1956a6849cec4d021d096b
John Edwards <edwards1860@gmail.com>
2024-12-18 19:44:33 -0500

    day 17 solved wooooooo

--- day17.jl
+++ day17.jl
@@ -1,71 +1,48 @@
 # input 
 include("Utils.jl")
 using .Utils
+input = get_input(2024,17)
+A = parse(Int64, input[1][3])
+B = parse(Int64, input[2][3])
+C = parse(Int64, input[3][3])
+prog = parse.(Int64, split(input[4][2],","))
 # part one
-function computer(A::Int, prog::Vector{Int})
-    reg = Dict("A" => A,"B" => 0,"C" => 0)
-    combo = Dict(0=>0,1=>1,2=>2,3=>3,4=>reg["A"],5=>reg["B"],6=>reg["C"])
+function computer(A, prog)
+    combo = Dict(0=>0,1=>1,2=>2,3=>3,4=>A,5=>B,6=>C)
     pointer = 0
     vals = []
     while true
-        if pointer + 1 > length(prog)
-            break
-        end
+        if pointer + 1 > length(prog) break end
         jump_pointer = true
-        opcode = prog[pointer+1]
-        operand = prog[pointer+2]
-        println(pointer)
-        println(reg)
-        if opcode == 0
-            reg["A"] = Int(floor(reg["A"] / 2 ^ combo[operand]))
-        elseif opcode == 1
-            reg["B"] = reg["B"] ⊻ operand
-        elseif opcode == 2
-            reg["B"] = combo[operand] % 8
-        elseif (opcode == 3) && (reg["A"] != 0)
+        opcode,operand = prog[pointer+1:pointer+2]
+        if opcode == 0 combo[4] = Int(floor(combo[4] / 2 ^ combo[operand]))
+        elseif opcode == 1 combo[5] = combo[5] ⊻ operand
+        elseif opcode == 2 combo[5] = combo[operand] % 8
+        elseif (opcode == 3) && (combo[4] != 0)
             pointer = operand
             jump_pointer = false
-        elseif opcode == 4
-            reg["B"] = reg["B"] ⊻ reg["C"]
-        elseif opcode == 5
-            append!(vals,combo[operand] % 8)
-        elseif opcode == 6
-            reg["B"] = Int(floor(reg["A"] / 2 ^ combo[operand]))
-        elseif opcode == 7
-            reg["C"] = Int(floor(reg["A"] / 2 ^ combo[operand]))
-        end
-        combo = Dict(0=>0,1=>1,2=>2,3=>3,4=>reg["A"],5=>reg["B"],6=>reg["C"])
-        if jump_pointer
-            pointer = pointer + 2
-        end
+        elseif opcode == 4 combo[5] = combo[5] ⊻ combo[6]
+        elseif opcode == 5 append!(vals,combo[operand] % 8)
+        elseif opcode == 6 combo[5] = Int(floor(combo[4] / 2 ^ combo[operand]))
+        elseif opcode == 7 combo[6] = Int(floor(combo[4] / 2 ^ combo[operand])) end
+        pointer = pointer + 2 * jump_pointer
     end
     return vals
 end
-computer(117440, [0,3,5,4,3,0])
+println(computer(A, prog))
 # part two
-function inverse_computer(prog)
-    combo = Dict(0=>0,1=>1,2=>2,3=>3,4=>missing,5=>missing,6=>missing)
-    operator_ndx = findfirst(x->(prog[x] == 5) & (x % 2 == 1),[i for i in eachindex(prog)]) - 2
-    y = length(prog)
-    while y > 0
-        p = prog[y]
-        if (operator_ndx < 1) && (y != 1) & (reg["A"] != 0)
-            operator_ndx = findfirst(x->(prog[x] == 3) & (x % 2 == 1),[i for i in eachindex(prog)])
-            operator_ndx = operator_ndx - 2
-        end
-        if operator_ndx >= 1
-            opcode = prog[operator_ndx]
-            operand = prog[operator_ndx + 1]
-            if opcode == 5
-                combo[operand] = p
-                reg = Dict("A"=>combo[4],"B"=>combo[5],"C"=>combo[6])
-                y = y - 1
-                operator_ndx = findfirst(x->(prog[x] == 5) & (x % 2 == 1),[i for i in eachindex(prog)]) - 2
-            elseif opcode == 0
-                reg["A"] = reg["A"] * 2 * combo[operand]
-                combo[4] = reg["A"]
-                operator_ndx = operator_ndx - 2
-            end
-        end
+function find_quine(prog)
+    combo = Dict(0=>0,1=>1,2=>2,3=>3,4=>A,5=>B,6=>C)
+    adv_ndx = findfirst(x->(prog[x]==0) & (x % 2 == 1), eachindex(prog))
+    multiplier = 2 ^ combo[prog[adv_ndx + 1]]
+    counter, prog_counter = 0,0
+    while prog_counter < length(prog)
+        if computer(counter, prog) == prog break
+        elseif computer(counter, prog) == prog[end-prog_counter:end]
+            counter = counter * multiplier
+            prog_counter = prog_counter + 1
+        else counter = counter + 1 end
     end
+    return counter
 end
+println(find_quine(prog))
